c8d3a6c638a4d7f708d46e098f6fe228aaf83e70
Patrick_Zimmermann <patrick_zimmermann@swissre.com>
2022-10-07 14:32:25 +0200

    Closes #2

---
+++ src/test/java/com/swissre/wxd/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swissre.wxd.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
